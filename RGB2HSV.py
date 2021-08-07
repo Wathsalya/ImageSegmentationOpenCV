5deb9d346be967e488d672dc1721defe9bf1244c
unknown <you@example.com>
2021-08-07 16:26:42 +0530

    first commit new

---
+++ RGB2HSV.py
@@ -0,0 +1,84 @@
+
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+
+from mpl_toolkits.mplot3d import Axes3D  # noqa
+from matplotlib.colors import hsv_to_rgb
+
+# To get a list of all the possible colour conversions
+
+flags = [i for i in dir(cv2) if i.startswith("COLOR_")]
+print(len(flags), "flags total:")
+
+print(flags[40])
+
+# Let's look at our fish image
+
+nemo = cv2.imread("./images/nemo0.jpg")
+plt.imshow(nemo)
+plt.show()
+
+# OpenCV by default opens images in BGR color space
+
+nemo = cv2.cvtColor(nemo, cv2.COLOR_BGR2RGB)
+
+plt.imshow(nemo)
+plt.show()
+
+# Plotting the image on 3D plot
+
+r, g, b = cv2.split(nemo)
+
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d")
+pixel_colors = nemo.reshape((np.shape(nemo)[0] * np.shape(nemo)[1], 3))
+norm = colors.Normalize(vmin=-1.0, vmax=1.0)
+norm.autoscale(pixel_colors)
+pixel_colors = norm(pixel_colors).tolist()
+
+axis.scatter(
+    r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker="."
+)
+axis.set_xlabel("Red")
+axis.set_ylabel("Green")
+axis.set_zlabel("Blue")
+plt.show()
+
+hsv_nemo = cv2.cvtColor(nemo, cv2.COLOR_RGB2HSV)
+
+h, s, v = cv2.split(hsv_nemo)
+
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+axis.scatter(
+    h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker="."
+)
+axis.set_xlabel("Hue")
+axis.set_ylabel("Saturation")
+axis.set_zlabel("Value")
+plt.show()
+
+light_orange = (1, 190, 200)## mask of green (36,25,25) ~ (86, 255,255)
+dark_orange = (18, 255, 255)
+
+lo_square = np.full((10, 10, 3), light_orange, dtype=np.uint8) / 255.0
+do_square = np.full((10, 10, 3), dark_orange, dtype=np.uint8) / 2
+plt.subplot(1, 2, 1)
+plt.imshow(hsv_to_rgb(do_square))
+plt.subplot(1, 2, 2)
+plt.imshow(hsv_to_rgb(lo_square))
+plt.show()
+
+mask = cv2.inRange(hsv_nemo, light_orange, dark_orange)
+result = cv2.bitwise_and(nemo, nemo, mask=mask)
+
+plt.subplot(1, 2, 1)
+plt.imshow(mask, cmap="gray")
+plt.subplot(1, 2, 2)
+plt.imshow(result)
+plt.show()
